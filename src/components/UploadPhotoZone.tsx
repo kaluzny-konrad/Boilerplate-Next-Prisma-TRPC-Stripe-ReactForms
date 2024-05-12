24cf3a558db863999013a9ac3e96b99b0404798c
Konrad Kałużny <konrad.kaluzny@hotmail.com>
2024-05-12 16:22:47 +0200

    add drag and drop styles

--- src/components/UploadPhotoZone.tsx
+++ src/components/UploadPhotoZone.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { UploadButton } from "@/lib/uploadthing";
+import { UploadButton, UploadDropzone } from "@/lib/uploadthing";
 import { Photo } from "@prisma/client";
 import { toast } from "sonner";
 
@@ -10,27 +10,25 @@ type Props = {
 
 export default function UploadPhotoZone({ handlePhotoUploaded }: Props) {
   return (
-    <div className="m-8 border-dashed border-2">
-      <UploadButton
-        className="w-full p-8"
-        endpoint="photoUploader"
-        onClientUploadComplete={(res) => {
-          if (typeof res === "undefined") return;
-          const photo = res[0].serverData?.photo;
-          if (typeof photo === "undefined")
+    <UploadDropzone
+      className="w-full p-8"
+      endpoint="photoUploader"
+      onClientUploadComplete={(res: any) => {
+        if (typeof res === "undefined") return;
+        const photo = res[0].serverData?.photo;
+        if (typeof photo === "undefined") return;
+        handlePhotoUploaded(photo);
+      }}
+      onUploadError={(error: Error) => {
+        if (typeof error.message === "string") {
+          if ((error.message = "Invalid config: FileSizeMismatch")) {
+            toast.error("File size too large. Please upload a smaller file.");
             return;
-          handlePhotoUploaded(photo);
-        }}
-        onUploadError={(error: Error) => {
-          if(typeof error.message === "string") {
-            if(error.message = "Invalid config: FileSizeMismatch") {
-              toast.error("File size too large. Please upload a smaller file.");
-              return;
-            }
           }
-          toast.error(`Something went wrong.`);
-        }}
-      />
-    </div>
+        }
+        toast.error(`Something went wrong.`);
+      }}
+    >
+    </UploadDropzone>
   );
 }
