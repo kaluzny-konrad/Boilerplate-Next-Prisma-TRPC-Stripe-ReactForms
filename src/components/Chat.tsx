0d3d90777f1dd437f0179973253968caeb1ffe97
Konrad Kałużny <konrad.kaluzny@hotmail.com>
2024-05-20 19:45:43 +0200

    add some space

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -22,7 +22,7 @@ export default function Chat({ id }: Props) {
 
   return (
     <ChatContextProvider chatId={id}>
-      <div className="relative flex flex-col justify-between min-h-full gap-2 pt-24 divide-y bg-zinc-50 divide-zinc-200">
+      <div className="relative flex flex-col justify-between min-h-full gap-2 pt-24 divide-y bg-zinc-50 divide-zinc-200 m-4">
         <div className="flex flex-col justify-between flex-1 mb-28">
           <ChatMessages chatId={id} />
         </div>
