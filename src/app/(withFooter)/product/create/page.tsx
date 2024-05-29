cca878942cd26179d0c5b5568270b2ac2c5ba43a
Konrad Kałużny <konrad.kaluzny@hotmail.com>
2024-05-29 16:12:03 +0200

    add admin route

--- src/app/(withFooter)/product/create/page.tsx
+++ src/app/(withFooter)/product/create/page.tsx
@@ -1,9 +1,15 @@
 import CreateProductForm from "@/components/Product/ProductCreateForm";
+import { redirect } from "next/navigation";
+import { checkRole } from "@/utils/roles";
 
 export default function ProductCreatePage() {
+  if (!checkRole("admin")) {
+    redirect("/");
+  }
+
   return (
     <div>
-        <CreateProductForm />
+      <CreateProductForm />
     </div>
-  )
+  );
 }
