86ef3b946d446be27536bced360060c96b7076e3
Konrad Kałużny <konrad.kaluzny@hotmail.com>
2024-06-07 18:38:17 +0200

    add reset db

---
+++ prisma/reset-db.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from "@prisma/client";
+import { createSeedClient } from "@snaplet/seed";
+
+const prisma = new PrismaClient();
+
+const seed = async () => {
+  try {
+    const seed = await createSeedClient();
+
+    await seed.$resetDatabase();
+
+    console.log("Database reseted successfully!");
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await prisma.$disconnect();
+    process.exit();
+  }
+};
+
+seed();
